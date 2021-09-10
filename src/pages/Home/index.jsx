a8eee4b9292b58efb8be6910374d333dab3d5a10
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-10 18:18:22 -0300

    Carrossel - Parte 2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
+import Slider from 'react-slick';
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
 
-import { Container, Search, Logo, Wrapper, Map } from './styles';
+import { Container, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true
+    };
+
     return (
         <Wrapper>
             <Container>
@@ -22,6 +34,21 @@ const Home = () => {
                             onChange={(e) => setInputValue(e.target.value)}
                         />
                     </TextField>
+                    <CarouselTitle>Na sua área</CarouselTitle>
+                    <Slider {...settings}>
+                        <div>
+                            <img src={restaurante} />
+                        </div>
+                        <div>
+                            <img src={restaurante} />
+                        </div>
+                        <div>
+                            <img src={restaurante} />
+                        </div>
+                        <div>
+                            <img src={restaurante} />
+                        </div>
+                    </Slider>
                 </Search >
             </Container >
             <Map />
