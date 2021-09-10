dba9249832f38c8c6bd1172b60cc3d8d62c4580a
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-10 16:53:19 -0300

    Conteúdo lateral - Última parte

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
 
-const Home = () => <Container>Hello world!</Container>
+import logo from '../../assets/logo.svg';
+
+import { Container, Search } from './styles';
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+    return (
+        <Container>
+            <Search>
+                <img src={logo} alt='logo do restaurante' />
+                <TextField
+                    label="Pesquisar"
+                    outlined
+                // onTrailingIconSelect={() => this.setState({value: ''})}
+                // </Search>trailingIcon={<MaterialIcon role="button" icon="delete" />}
+                >
+                    <Input
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                    />
+                </TextField>
+            </Search>
+        </Container>
+    )
+};
 
 export default Home;
