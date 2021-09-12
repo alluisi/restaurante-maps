978c6b9b83c1f24a28fb8643672d4417735650a9
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-11 22:21:26 -0300

    Skeleton - Última parte

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactStars from 'react-rating-stars-component';
 
 import restaurante from '../../assets/restaurante-fake.png';
 
 import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles';
+import { Skeleton } from '../Skeleton';
 
-const ResturantCard = (restaurant, onClick) => (
-  <Restaurant onClick={onClick}>
-    <RestaurantInfo>
-      <Title>{restaurant.name}</Title>
-      <ReactStars
-        count={5}
-        isHalf
-        value={restaurant.rating}
-        edit={false}
-        activeColor="#E7711C"
+const ResturantCard = (restaurant, onClick) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  return (
+    <Restaurant onClick={onClick}>
+      <RestaurantInfo>
+        <Title>{restaurant.name}</Title>
+        <ReactStars
+          count={5}
+          isHalf
+          value={restaurant.rating}
+          edit={false}
+          activeColor="#E7711C"
+        />
+        <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
+      </RestaurantInfo>
+      <RestaurantPhoto
+        imageLoaded={imageLoaded}
+        src={restaurant.photos ? restaurant.photos[0].getUrl() : restaurante}
+        onLoad={() => setImageLoaded(true)}
+        alt="Foto do restaurante"
       />
-      <Address>{restaurant.vicinity || restaurant.formatted_address}</Address>
-    </RestaurantInfo>
-    <RestaurantPhoto
-      src={restaurant.photos ? restaurant.photos[0].getUrl() : restaurante}
-      alt="Foto do restaurante"
-    />
-  </Restaurant>
-);
+      {!imageLoaded && <Skeleton width='100px' height='100px' />}
+    </Restaurant>
+  )
+};
 
 export default ResturantCard;
